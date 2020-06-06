904a8ac66690d598e14def8d8371176be18b647d
Natividad Alvarez <tivialvarez@gmail.com>
2020-06-05 22:31:04 -0400

    Completed Menu.js step 4

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -49,3 +49,8 @@ function menuMaker(menuItems){
 }
 
 const menuButton = document.querySelector(".menu-button");
+
+menuButton.addEventListener("click", ()=>{
+  const menu = document.querySelector("menu");
+  menu.classList.toggle("menu--open");
+})
